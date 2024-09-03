19fa6ffb04f7ccb8844708651df9569518cf164e
Olavo Borges <olavoborges@MacBook-Pro-de-Olavo.local>
2024-09-03 11:48:51 -0300

    new app

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    st.run(port=port)
+
 import streamlit as st
 
-st.title('Hello, World!')
-st.write('Welcome to your first Streamlit app on IBM Cloud Code Engine!')
+st.title("Hello World App")
+st.write("Hello, World! This is a simple Streamlit app deployed on IBM Cloud.")
 
